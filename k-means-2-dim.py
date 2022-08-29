50e18a365be1d7f850ab3762e21f9054bc147606
Eskil <eskped@gmail.com>
2022-08-29 15:28:39 +1000

    Finished k-means script

--- k-means-2-dim.py
+++ k-means-2-dim.py
@@ -1,6 +1,9 @@
 import random
 
 all_points = [(2, 2), (4, 6), (4, 8), (6, 6), (6, 8), (8, 0)]
+number_of_clusters = 3
+number_of_different_start_clusters = 5
+number_of_iterations_per_cluster = 5
 
 
 def manhattan_distance(p1, p2):
@@ -11,65 +14,69 @@ def find_closest(p, points):
     return min(points, key=lambda point: manhattan_distance(point, p))
 
 
-def update_centroids(points, mean):
-    print("hei")
-    print(mean)
+def find_random_centroids(points, k):
+    centroids = random.sample(points, k)
+    return centroids
+
+
+def get_new_centroids(mean):
+
     new_keys = []
-    old_keys = [mean.keys()]
+    old_keys = list(mean.keys())
     for centroid in old_keys:
         sum = [0, 0]
-        # print(mean)
         if len(mean[centroid]) == 0:
-            continue
+            new_keys.append(centroid)
         else:
             for point in mean[centroid]:
                 sum[0] += point[0]
                 sum[1] += point[1]
-            new_keys.appen(sum[0] / len(mean[centroid]), sum[1] /
-                           len(mean[centroid]))
-    for i in range(len(new_keys)):
-        # mean[new_key[i]] = mean[old_key[i]]
-        # del dictionary[old_key[i]]
-        mean[mean[i]] = mean.pop(centroid[i])
+            new_keys.append(
+                (round(sum[0] / len(mean[centroid]), 5), round(sum[1] / len(mean[centroid]), 5)))
+    new_mean = {}
+    for i in new_keys:
+        new_mean[i] = []
 
-    return mean
+    return new_mean
 
 
-def k_means(points, k):
-    centroids = random.sample(points, k=k)
-    mean = {}
-    for centroid in centroids:
-        mean[centroid] = []
-    for i in range(2):
-        points_copy = points.copy()
-        points_copy = [
-            point for point in points_copy if point not in mean.keys()]
-        # print(mean)
-        print(points_copy)
-        for point in points_copy:
-            # print("point", point)
-            distance = 999999
-            for centroid in mean.keys():
-                if manhattan_distance(point, centroid) < distance:
-                    distance = manhattan_distance(point, centroid)
-                    closest_centroid = centroid
-            if closest_centroid in mean.keys():
+def SSE(mean):
+    sse = 0
+    for centroid in mean.keys():
+        for point in mean[centroid]:
+            sse += (manhattan_distance(point, centroid))**2
+    return sse
+
+
+def k_means(points, k, different_start_clusters, number_of_iterations):
+    best_try = [99999, {}]
+    for j in range(different_start_clusters):
+        centroids = find_random_centroids(points, k)
+        mean = {}
+        for centroid in centroids:
+            mean[centroid] = []
+        for i in range(number_of_iterations):
+            points_copy = points.copy()
+            mean = get_new_centroids(mean)
+            for point in points_copy:
+                distance = 999999
+                for centroid in mean.keys():
+                    if manhattan_distance(point, centroid) < distance:
+                        distance = manhattan_distance(point, centroid)
+                        closest_centroid = centroid
                 mean[closest_centroid].append(point)
-            else:
-                mean[closest_centroid] = [point]
-        # print(mean.keys())
-        # print(centroids)
-        print(mean)
-        print(update_centroids(points_copy, mean))
+        if SSE(mean) < best_try[0]:
+            best_try = [SSE(mean), mean]
 
-        # if all(closest_points[p] == clusters[p] for p in points):
-        #     return clusters
-        # for p in points:
-        #     clusters[p] = closest_points[p]
+    return best_try[0], best_try[1]
 
 
 def main():
-    k_means(all_points, 3)
+    sse, mean = k_means(all_points, number_of_clusters, number_of_different_start_clusters,
+                        number_of_iterations_per_cluster)
+
+    print(sse)
+    print(mean)
 
 
 main()
